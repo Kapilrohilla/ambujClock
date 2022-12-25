b7963ec6e6f4c9c9d283098ed1c08e09e0d4a19c
kapilRohilla <kapilrohilla2002@gmail.com>
2022-12-25 13:24:55 +0530

    git push .gitignore git push css/style.css git push css/style.css.map git push css/style.scss git push index.html git push js/script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const hrs = document.querySelector('.hours');
+const min = document.querySelector('.minute');
+const sec = document.querySelector('.second');
+const color = document.querySelector('.color');
+
+function time(){
+    let h,m,s;
+    let dt = new Date();
+    h = dt.getHours();
+    m = dt.getMinutes();
+    s = dt.getSeconds();
+
+    hrs.innerHTML = h;
+    min.innerHTML = m;
+    sec.innerHTML = s;
+
+    if(h < 10){
+        hrs.innerHTML = "0" + h;
+    } else if(m < 10){
+        min.innerHTML = "0" + m;
+    } else if(s < 10){
+        sec.innerHTML = "0" + s;
+    }
+
+}
+function chColor(){
+    let r,g,b,rgb,ln_gd;
+    
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+    rgb = 'rgb('+r+','+g+','+b+')';
+
+    ln_gd = 'linear-gradient(to bottom,#19282c,'+rgb+')';
+    color.style.background = ln_gd;
+}
+setInterval(chColor,1000);
+setInterval(time,10);
